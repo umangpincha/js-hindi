53651bda58c5ce34ecbe34fc2a6d47bd14871248
Umang Pincha <umangpincha@gmail.com>
2023-09-24 13:09:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,46 @@
+// let score = "33";
+// let score2 = "33www";
+// // let score3 = null;
+// let score4 = undefined;
+// // let score5 = true;  
+// // let score6 = "Umang";  
+// // console.log(score6);
+// // console.log(typeof(score));
+// console.log(score4);
+
+// // score = Number(score);
+// // score2 = Number(score2);
+// score4 = Number(score4);
+// score5 = Number(score5) //true = 1 , false=0
+// score6 = Number(score6)
+
+// console.log(typeof(score));
+// console.log(typeof(score2));
+// console.log(typeof(score3));
+// console.log(typeof(score4));
+// console.log(typeof(score5));
+// console.log(typeof(score6));
+// console.log(score2);
+// console.log(score3);
+// console.log(score4);
+// console.log(score5);
+// console.log(score6);
+
+
+// conversion to number
+
+// "33"=>33
+// null=>NaN
+// "33aas"=>NaN
+// "Umang" => NaN
+// true => 1 , false => 0
+// undefined => undefined
+
+
+// Conversion to boolean
+let isLoggedIn = "";
+isLoggedIn = Boolean(isLoggedIn); //false
+console.log(isLoggedIn);
+console.log(typeof(isLoggedIn));
+
+// "" => false , "Umang" => true
