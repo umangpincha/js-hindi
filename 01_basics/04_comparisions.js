3a0578e2a353d9218db92e80000f2d61851fa6d0
Umang Pincha <umangpincha@gmail.com>
2023-09-25 04:02:40 +0000

    03_04 files comitted

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,22 @@
+
+// NOTE : Don't ever try ot do these type of conversions in production , clean code is always prefered 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+// Equaliity (==) , comparision (> , < , >= , <=) work differently .
+// Comparision converst null to a number treating it as 0 
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+
+// undefined always gives false as output
+// console.log(undefined > 0);
+// console.log(undefined == 0); 
+// console.log(undefined >= 0); 
+
+console.log("2" == 2);
+console.log("2" === 2);
